1b0f8495968d5548d016798920bf203098ca55ed
Usman Khan <muhammadusmankhan307@gmail.com>
2019-07-03 01:35:51 +0500

    Claim Based Auth in Api in module-02.

---
+++ module-02/ClaimBasedAuthAPI/ClaimBasedAuth/Controllers/AuthController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using ClaimBasedAuth.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ClaimBasedAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _roleManager = roleManager;
+
+        }
+        [Route("register")]
+        [HttpPost]
+        public async Task<ActionResult> InsertUser([FromBody] RegisterViewModel model)
+        {
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var role = new IdentityRole("Admin");
+                await _roleManager.CreateAsync(role);
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+            return Ok(new { Username = user.UserName });
+        }
+
+        //          /login
+        [Route("login")]
+        [HttpPost]
+        public async Task<ActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+             //   var claim = new[] {
+              //      new Claim(JwtRegisteredClaimNames.Sub, user.UserName)
+               // };
+             var claim = new List<Claim>();
+                   claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+                var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+
+                int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+                var token = new JwtSecurityToken(
+                  issuer: _configuration["Jwt:Site"],
+                  audience: _configuration["Jwt:Site"],
+                  expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256),
+                  claims:claim
+                );
+
+                return Ok(
+                  new
+                  {
+                      token = new JwtSecurityTokenHandler().WriteToken(token),
+                      expiration = token.ValidTo
+                  });
+            }
+            return Unauthorized();
+        }
+
+    }
+}
